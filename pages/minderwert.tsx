8c687f7d283b7abdd5218110173769dbbc250e1a
Hoid <github@oedon.me>
2025-10-10 14:03:09 +0200

    fix: icon

--- pages/minderwert.tsx
+++ pages/minderwert.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useState, useEffect, Fragment } from 'react'
+import { FaBalanceScale } from 'react-icons/fa'
 import { useScreenshot } from '../hooks/useScreenshot'
 
 interface TooltipProps {
@@ -1170,9 +1171,7 @@ function Minderwert() {
             {/* Enhanced Progress Bars */}
             <div className="bg-white rounded-xl p-6 shadow-sm border border-gray-100">
               <h5 className="text-base font-semibold text-gray-700 mb-6 flex items-center gap-2">
-                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"/>
-                </svg>
+                <FaBalanceScale className="w-5 h-5" />
                 Proportionaler Vergleich
               </h5>
               <div className="space-y-5">
